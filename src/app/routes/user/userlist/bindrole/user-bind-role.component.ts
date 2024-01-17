a66db8a1064f0b36e6abef60a9199076894991d5
foticc <foticc@163.com>
2024-01-17 18:38:27 +0800

    //add mock api

---
+++ src/app/routes/user/userlist/bindrole/user-bind-role.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-bind-role',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-bind-role.component.html'
+})
+export class UserBindRoleComponent {}
